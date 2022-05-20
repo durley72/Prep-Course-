7f3b54cd16027bb5540def78b6a15ddc2703437b
durley72 <jamescastano0663@gmail.com>
2022-05-19 18:38:15 -0600

    esplicacion

---
+++ 05-JS-IV/homework/homework05.js
@@ -0,0 +1,15 @@
+homework05.js
+Objetos :
+es una coleccion de propiedades en js
+
+Propiedades :
+es la asosiacion entre el nombre key:value
+
+Métodos :
+son las funciones guardadas en un objeto
+
+Bucle for…in :
+sirve para unir vrios objetos 
+
+Notación de puntos vs notación de corchetes . 
+SON ELEMENTOS QUE SE USAN EN un objetos
